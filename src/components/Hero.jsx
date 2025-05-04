1a19e532dc88ffbe24281cf02f54692e57fccb01
Nirit Nagar <nirit.nn@gmail.com>
2025-05-04 19:31:57 +0200

    add btn to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,23 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSlider } from "../context/SliderContext";
-import { Volume2, VolumeX } from "lucide-react";
+import { Mail, MailOpen } from "lucide-react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const { hue, saturation, brightness, opacity } = useSlider();
   const [loading, setLoading] = useState(true);
-  const audioElementRef = useRef(null);
+  const [showSend, setShowSend] = useState(false);
+  const handleMouseEnter = () => setShowSend(true);
+  const handleMouseLeave = () => setShowSend(false);
+
+  const mailIconClass = showSend ? "opacity-0" : "opacity-100";
+  const mailOpenIconClass = showSend ? "opacity-100" : "opacity-0";
+  const textClass = showSend ? "opacity-0" : "opacity-100";
+  const textClassActive = showSend ? "opacity-100" : "opacity-0";
 
   const overlayStyle = useMemo(
     () => `hsla(${hue}, ${saturation}%, ${brightness}%, ${opacity / 100})`,
@@ -24,6 +31,11 @@ const Hero = () => {
     }
     return `hsl(${hue}, 100%, ${Math.min(95, Math.max(5, lightness))}%)`;
   }, [hue, brightness]);
+
+  const sendMail = () => {
+    window.location.href =
+      "mailto:nirit.nagar.dev@gmail.com?subject=Let's Connect – Opportunity to Collaborate";
+  };
 
   useEffect(() => {
     const video = document.getElementById("hero-video");
@@ -96,7 +108,7 @@ const Hero = () => {
           <div className="flex size-full flex-col items-center justify-center">
             <div className="flex items-baseline justify-center">
               <h1
-                className="cursor-default font-fancy text-[19vw] font-medium leading-none"
+                className="cursor-default font-fancy text-[19vw] max-sm:text-[22vw] font-medium leading-none"
                 style={{ color: textColor }}
               >
                 CreaDEV.
@@ -104,11 +116,42 @@ const Hero = () => {
             </div>
 
             <p
-              className="text-center cursor-default mb-10 font-general text-2xl tracking-widest"
+              className="text-center cursor-default mb-10 max-sm:mb-6 font-general text-2xl tracking-widest"
               style={{ color: textColor }}
             >
               Nirit Nagar :: Creative Development
             </p>
+            <button
+              onClick={sendMail}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className="line font-medium text-white bg-transparent border-2 rounded-full w-auto flex py-1 sm:py-4 pr-12 pl-14 max-sm:px-4 max-sm:mt-0 items-center justify-center"
+              style={{ borderColor: "rgba(255, 255, 255, 0.5)" }}
+              aria-label="Send email"
+              role="button"
+            >
+              <span className="mr-1 size-[40px] pl-2 sm:pl-0 mt-0 relative">
+                <Mail
+                  className={`${mailIconClass} size-[28px] sm:size-[34px] absolute top-1/2 transform -translate-y-1/2 transition-opacity duration-500`}
+                />
+                <MailOpen
+                  className={`${mailOpenIconClass} size-[28px] sm:size-[34px] absolute left top-1/2 transform -translate-y-1/2 transition-opacity duration-500`}
+                />
+              </span>
+              <span className="text-white text-[7vw] sm:text-[2vw] transition-opacity duration-500">
+                Let's connect
+                <span
+                  className={`${textClass} transition-opacity duration-500`}
+                >
+                  !
+                </span>
+                <span
+                  className={`${textClassActive} transition-opacity duration-500`}
+                >
+                  {":)"}
+                </span>
+              </span>
+            </button>
           </div>
         </div>
       </div>
